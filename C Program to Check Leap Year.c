e4f1f3a5bd6c06598bfc26c24d25d7baeb05c4a5
Khandaker-Afif <143590111+Khandaker-Afif@users.noreply.github.com>
2023-08-30 14:01:56 +0600

    C Program to Check Leap Year.c

--- C Program to Check Leap Year.c
+++ C Program to Check Leap Year.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+main(){
 
+    int input;
+
+    printf("Enter the year in numbers:");
+    scanf("%d",&input);
+
+
+    if( input%400==0 || ( input%4==0 && input%100!=0)){
+
+        printf("The year you entered is a leap year!");
+    }
+    else {
+
+        printf("The year you entered is not a leap year...:-(");
+    }
+
+
+
+}
