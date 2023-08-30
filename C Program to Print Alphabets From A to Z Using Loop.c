55682386277eae65b72d5401cb8306b3d69d8ed7
Khandaker-Afif <143590111+Khandaker-Afif@users.noreply.github.com>
2023-08-30 13:57:48 +0600

    printing alphabets from A to Z using a loop.c

---
+++ C Program to Print Alphabets From A to Z Using Loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main(){
+
+    char alphabet;
+
+    printf("The English alphabets in uppercase letters from A to Z are:");
+
+    for(int i=65;i<=90;i++){
+
+        alphabet=i;
+        printf("%c,",alphabet);
+    }
+
+
+}
